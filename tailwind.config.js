989c3c28e831bbad14b46afcb046c6bf0981d656
baronrafael <baronrafael@hotmail.com>
2021-01-22 13:58:03 -0400

    fix: purge content for budgets

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,8 @@
 module.exports = {
-  purge: ['./src/**/*.html', './src/**/*.ts'],
+  purge: {
+    enabled: true,
+    content: ['./src/**/*.{html,ts}']
+  },
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {},
